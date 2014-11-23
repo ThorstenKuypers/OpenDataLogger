e63c8c873fbe85c5998c4e172dc22069990e1f12
ThorstenKuypers <v8judd@gmail.com>
2014-11-23 16:20:22 +0100

    v0.3.1
    + added GPL v3 license
    + changed project settings to enable optimizations in Release
    configurations

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,15 +2,16 @@
 OpenDataLogger - project structure
 
 
-  openDataLogger: Data set "wrapper" module. It implements a unified set of telemetry
-                  data objects which are then fed into the openGameAPI module. It is
-				  implemented as a static library against which all plugins are linked 
-				  at compile time.
+openDataLogger: Data set "wrapper" module. It implements a unified set of
+			    telemetry data objects which are then fed into the openGameAPI 
+				module. It is implemented as a static library against which all
+				plugins are linked at compile time.
 
-  openGameAPI: Is the core data logger "device". Provides a shared memory "live-logger"
-	       and disk-based (file) telemetry data logging functionality.
+openGameAPI: Is the core data logger "device". Provides a shared memory 
+			 "live-logger" and disk-based (file) telemetry data logging
+			 functionality.
 
-  plugins:
-  ---------
-  Actual interfaces which integrate into the corresponding sim to provide sim-specific
-  telemetry data.
+plugins:
+---------
+Actual interfaces which integrate into the corresponding sim to provide sim-specific
+telemetry data.
--- src/sims/rF2/rf2Plugin.h
+++ src/sims/rF2/rf2Plugin.h
@@ -1,4 +1,25 @@
-#pragma once
+#ifndef _RF2_PLUGIN_H_
+#define _RF2_PLUGIN_H_
+/*****************************************************************************
+** openDataLogger - implementation of openGameAPI for use in plugins for
+several racing simulations
+
+** (C) 2014 Thorsten Kuypers
+
+** This file is part of openDataLogger project.
+
+** openDataLogger is free software : you can redistribute it and / or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License.
+
+** openDataLogger is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+** GNU General Public License for more details.
+
+** You should have received a copy of the GNU General Public License
+** along with openDataLogger. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
 
 #include <OpenDataLogger.h>
 #include <BasePlugin.h>
@@ -46,3 +67,4 @@ private:
 	VehicleScoringInfoV01 _playerVehicleInfo; // VehicleScoringInfo of player
 };
 
+#endif // _RF2_PLUGIN_H_
